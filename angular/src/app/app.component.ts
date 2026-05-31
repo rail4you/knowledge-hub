4b019ab483dd0a60a601418e7de0b9bd7f0a72c7
bai <rail4you@gmail.com>
2026-06-01 07:40:32 +0800

    fix: 添加 CUSTOM_ELEMENTS_SCHEMA 解决 abp-loader-bar 编译错误

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { DynamicLayoutComponent } from '@abp/ng.core';
-import { LoaderBarComponent } from '@abp/ng.theme.shared';
 import { AuthErrorModalComponent } from './core/auth/auth-error-modal.component';
 
 @Component({
   selector: 'app-root',
   template: `
-    <abp-loader-bar />
-    <abp-dynamic-layout />
+    <abp-loader-bar></abp-loader-bar>
+    <abp-dynamic-layout></abp-dynamic-layout>
     <app-auth-error-modal></app-auth-error-modal>
   `,
-  imports: [LoaderBarComponent, DynamicLayoutComponent, AuthErrorModalComponent],
+  standalone: true,
+  imports: [DynamicLayoutComponent, AuthErrorModalComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {}
