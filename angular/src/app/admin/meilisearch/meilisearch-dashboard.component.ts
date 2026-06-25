92bc9a941ccdbb20716a5644b05ae3303657add1
bai <rail4you@gmail.com>
2026-06-25 08:55:48 +0800

    fix: 恢复 computed 导入

--- angular/src/app/admin/meilisearch/meilisearch-dashboard.component.ts
+++ angular/src/app/admin/meilisearch/meilisearch-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, inject, signal, computed, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LocalizationPipe, ConfigStateService } from '@abp/ng.core';
