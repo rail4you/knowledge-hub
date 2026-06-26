aee63803a56b52684f7eb49a82629c681383f9f6
bai <rail4you@gmail.com>
2026-06-26 14:40:03 +0800

    chore: 删除 Java 文档解析器相关文件
    移除 utils/pdf-parser/ (Java 源码、pom.xml、target/)
    移除 src/.../utils/pdf-parser/ (run-parser.sh, build.sh, jar, META-INF)
    这些文件已由 NpoiDocumentParserService + DocumentFormat.OpenXml 替代

--- src/KnowledgeHub.HttpApi.Host/utils/pdf-parser/run-parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper script to run document-parser.jar with proper environment
-export PATH="/opt/homebrew/bin:$PATH"
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8"
-exec java -Xmx256m -Xms128m -Dfile.encoding=UTF-8 -jar "$(dirname "$0")/document-parser.jar" "$@"
